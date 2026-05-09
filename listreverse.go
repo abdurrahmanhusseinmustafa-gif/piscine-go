d9bebe1aec052bfeebe4249b9b0c10048a7b6a33
amustafa <kambiomarketing.com>
2026-05-09 16:48:41 +0100

    listreverse.go

---
+++ listreverse.go
@@ -0,0 +1,17 @@
+package piscine
+
+func ListReverse(l *List) {
+	if l.Head == nil {
+		return
+	}
+	var prev *NodeL
+	current := l.Head
+	l.Tail = l.Head
+	for current != nil {
+		next := current.Next
+		current.Next = prev
+		prev = current
+		current = next
+	}
+	l.Head = prev
+}
